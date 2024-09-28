6c8d581704d3289f9176a36069b14850c6088d59
Muhammad Nasrulloh <muhammadnasrulloh.id@gmail.com>
2024-09-28 17:53:06 +0700

    update

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -13,7 +13,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
+      callbackURL: `http://${process.env.NODE_ENV === 'production' ? 'www.ditopupin.com' : 'localhost:3000'}/api/auth/google/callback`,
       scope: ['email', 'profile'],
     });
   }
